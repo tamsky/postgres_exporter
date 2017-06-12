792eb3e2d74c814678fade9a82093e6e2a3fbef0
Marc Tamsky <tamsky@users.noreply.github.com>
2017-06-11 22:24:26 -0700

    revert docker-entrypoint to master

--- tests/docker-postgres-replication/docker-entrypoint.sh
+++ tests/docker-postgres-replication/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Backwards compatibility for old variable names (deprecated)
-if [[ "$PGUSER" ]]; then
+if [ "x$PGUSER"     != "x" ]; then
     POSTGRES_USER=$PGUSER
 fi
-if [[ "$PGPASSWORD" ]]; then
+if [ "x$PGPASSWORD" != "x" ]; then
     POSTGRES_PASSWORD=$PGPASSWORD
 fi
 
 # Forwards-compatibility for old variable names (pg_basebackup uses them)
-if [[ -z "$PGPASSWORD" ]]; then
+if [ "x$PGPASSWORD" = "x" ]; then
     export PGPASSWORD=$POSTGRES_PASSWORD
 fi
 
@@ -18,11 +18,11 @@ fi
 
 set -e
 
-if [[ "${1:0:1}" = '-' ]]; then
+if [ "${1:0:1}" = '-' ]; then
 	set -- postgres "$@"
 fi
 
-if [[ "$1" = 'postgres' ]]; then
+if [ "$1" = 'postgres' ]; then
 	mkdir -p "$PGDATA"
 	chmod 700 "$PGDATA"
 	chown -R postgres "$PGDATA"
@@ -32,17 +32,16 @@ if [[ "$1" = 'postgres' ]]; then
 	chown -R postgres /run/postgresql
 
 	# look specifically for PG_VERSION, as it is expected in the DB dir
-	if [[ ! -s "$PGDATA/PG_VERSION" ]]; then
-	    if [[ -z "${REPLICATE_FROM}" ]]; then
+	if [ ! -s "$PGDATA/PG_VERSION" ]; then
+	    if [ "x$REPLICATE_FROM" == "x" ]; then
 		eval "gosu postgres initdb $POSTGRES_INITDB_ARGS"
 	    else
-            	until ping -c 1 -W 1 "${REPLICATE_FROM}"
+            	until ping -c 1 -W 1 ${REPLICATE_FROM}
             	do
                 	echo "Waiting for master to ping..."
                 	sleep 1s
             	done
-            	until gosu postgres pg_basebackup -h "${REPLICATE_FROM}" \
-                           -D "${PGDATA}" -U "${POSTGRES_USER}" -vP -w
+            	until gosu postgres pg_basebackup -h ${REPLICATE_FROM} -D ${PGDATA} -U ${POSTGRES_USER} -vP -w
             	do
                 	echo "Waiting for master to connect..."
                 	sleep 1s
@@ -51,7 +50,7 @@ if [[ "$1" = 'postgres' ]]; then
 
 		# check password first so we can output the warning before postgres
 		# messes it up
-		if [[ ! -z "$POSTGRES_PASSWORD" ]]; then
+		if [ ! -z "$POSTGRES_PASSWORD" ]; then
 			pass="PASSWORD '$POSTGRES_PASSWORD'"
 			authMethod=md5
 		else
@@ -74,60 +73,46 @@ if [[ "$1" = 'postgres' ]]; then
 			authMethod=trust
 		fi
 
+		if [ "x$REPLICATE_FROM" == "x" ]; then
 
-		if [[ -z ${REPLICATE_FROM} ]]; then
-			{ echo; echo "host replication all 0.0.0.0/0 $authMethod"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
-			{ echo; echo "host all all 0.0.0.0/0 $authMethod"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
+		{ echo; echo "host replication all 0.0.0.0/0 $authMethod"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
+		{ echo; echo "host all all 0.0.0.0/0 $authMethod"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
 
-			# internal start of server in order to allow set-up using psql-client
-			# does not listen on external TCP/IP and waits until start finishes
-			gosu postgres pg_ctl -D "${PGDATA}" \
-			     -o "-c listen_addresses='localhost'" \
-			     -w start
+		# internal start of server in order to allow set-up using psql-client		
+		# does not listen on external TCP/IP and waits until start finishes
+		gosu postgres pg_ctl -D "$PGDATA" \
+			-o "-c listen_addresses='localhost'" \
+			-w start
 
-		        : ${POSTGRES_USER:=postgres}
-		        : ${POSTGRES_DB:=$POSTGRES_USER}
-		        export POSTGRES_USER POSTGRES_DB
+		: ${POSTGRES_USER:=postgres}
+		: ${POSTGRES_DB:=$POSTGRES_USER}
+		export POSTGRES_USER POSTGRES_DB
 
-		        psql=( psql -v ON_ERROR_STOP=1 )
+		psql=( psql -v ON_ERROR_STOP=1 )
 
-		        if [[ "$POSTGRES_DB" != 'postgres' ]]; then
-			    "${psql[@]}" --username postgres <<-EOSQL
+		if [ "$POSTGRES_DB" != 'postgres' ]; then
+			"${psql[@]}" --username postgres <<-EOSQL
 				CREATE DATABASE "$POSTGRES_DB" ;
-				EOSQL
-			    echo
-			fi
-
-                        # Possible alternate flavor vs <<-HEREDOC :
-                        #
-			# if [[ "$POSTGRES_DB" != "postgres" ]]; then
-                        #     SQL=$(printf 'CREATE DATABASE "%s" ;' $POSTGRES_DB)
-			#     "${psql[@]}" --username postgres <( echo "${SQL}" )
-			#     echo
-			# fi
-
-			if [[ "$POSTGRES_USER" = 'postgres' ]]; then
-			    op='ALTER'
-			else
-			    op='CREATE'
-			fi
-		        "${psql[@]}" --username postgres <<-EOSQL
-				$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
-				EOSQL
+			EOSQL
 			echo
+		fi
 
-                        # SQL=$(printf '%s USER "%s" WITH SUPERUSER %s ;' \
-                        #              "$op" "$POSTGRES_USER" "$pass" )
-			# "${psql[@]}" --username postgres <( echo ${SQL} )
-				
-			echo
+		if [ "$POSTGRES_USER" = 'postgres' ]; then
+			op='ALTER'
+		else
+			op='CREATE'
+		fi
+		"${psql[@]}" --username postgres <<-EOSQL
+			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
+		EOSQL
+		echo
+		
 		fi
 
 		psql+=( --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" )
 
 		echo
 		for f in /docker-entrypoint-initdb.d/*; do
-                        # shellcheck disable=SC1090
 			case "$f" in
 				*.sh)     echo "$0: running $f"; . "$f" ;;
 				*.sql)    echo "$0: running $f"; "${psql[@]}" < "$f"; echo ;;
